6230d07951a28b24e8ec9ec459d1c7ac00aba73c
Garrett Tanzer <gtanzer@college.harvard.edu>
2017-09-29 12:46:48 -0400

    new file skeleton

---
+++ main_so/patcher.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#define UDS_NAME "patch_uds"
+
+int main(int argc, char **argv) {
+	
+	// --- send patch -----------------------------------------
+	
+	int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+	if(fd < 0) {
+		perror("socket() failed\n");
+		_exit(1);
+	}
+	
+	struct sockaddr_un server;
+	server.sun_family = AF_UNIX;
+	strcpy(server.sun_path, UDS_NAME); // don't make NAME too long
+	
+	int err = connect(fd, (struct sockaddr *) &server, sizeof(struct sockaddr_un));
+	if(err < 0) {
+		close(fd);
+		perror("connect() failed\n");
+		_exit(1);
+	}
+	
+	char *name;
+	if(argc == 2)
+		name = "patch2.dylib";
+	else
+		name = "patch.dylib";
+	size_t len = strlen(name);
+	
+	err = write(fd, (void *) &len, sizeof(size_t));
+	if(err < 0) {
+		close(fd);
+		perror("header write() failed\n");
+		_exit(1);
+	}
+	
+	err = write(fd, name, len);
+	if(err < 0) {
+		close(fd);
+		perror("body write() failed\n");
+		_exit(1);
+	}
+	
+	close(fd);
+	
+	return 0;
+}
